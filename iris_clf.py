8c22b24fed130917fb5c9eb1b01f70c3a58293b8
NguyenHoaiNgoc <164768732+NguyenHoaiNgoc@users.noreply.github.com>
2024-03-26 05:48:51 +0700

    commit2603

--- iris_clf.py
+++ iris_clf.py
@@ -1,29 +1,16 @@
 # 
-
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 import pickle
 
-def load_data():
-    iris = load_iris()
-    X = iris.data
-    y = iris.target
-    return X, y
+iris_data=load_iris()
+features=iris_data.data
+target=iris_data.target
 
-def train_model(X, y):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    
-    rf_clf = RandomForestClassifier(random_state=42)
-    rf_clf.fit(X_train, y_train)
-    
-    return rf_clf
+features_train,features_test,target_train,target_test=train_test_split(features,target,test_size=0.2)
+lrg=LogisticRegression()
+lrg.fit(features_train,target_train)
+score=lrg.score(features_test,target_test)
 
-def save_model(model, filename):
-    with open(filename, 'wb') as file:
-        pickle.dump(model, file)
-
-if __name__ == "__main__":
-    X, y = load_data()
-    model = train_model(X, y)
-    save_model(model, "iris_model_001.pkl")
+pickle.dump(lrg,open("iris_lrg_model01.pkl", 'wb'))
